af9c85bac82dfd050071d432edd776f3e44024fe
Elías Calderón <eliascr98@outlook.com>
2017-09-21 22:29:07 -0600

    Updates on classes and dia

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,21 @@
+
 public class Graph {
 
-    private
+    private boolean[][] asRelations;
+    private final int maximumASes = 10;
+
+    public Graph () {
+        this.asRelations = new boolean[maximumASes][maximumASes];
+    }
+
+    //------------------------------------------------------------------------------
+
+    public void add_asRelation (int asX_id, int asY_id) {
+        this.asRelations[asX_id][asY_id] = true;
+    }
+
+    public void remove_asRelation (int asX_id, int asY_id) {
+        this.asRelations[asX_id][asY_id] = false;
+    }
 
 }
