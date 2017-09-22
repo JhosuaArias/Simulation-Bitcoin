5aef7aa5325682e4bafbac0d69d6dfe81ce67719
JhosuaArias <jhosuarqui@hotmail.com>
2017-09-22 12:11:15 -0600

    Arreglar en lo que me cagué

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,46 @@
+import java.util.Stack;
+
 public class Node {
+
+    /*Attributes*/
+    private As asFather;
+    private double actualCurrency;
+    private Stack<Block> blockChain;
+
+    /*Constructor*/
+    public Node(As asFather) {
+        this.setAsFather(asFather);
+    }
+
+    /*Methods*/
+    private boolean informToAs() {
+        return false;
+    }
+
+    public As getAsFather() {
+        return asFather;
+    }
+
+    /*Getters and Setters*/
+
+    public void setAsFather(As asFather) {
+        this.asFather = asFather;
+    }
+
+    public double getActualCurrency() {
+        return actualCurrency;
+    }
+
+    public void setActualCurrency(double actualCurrency) {
+        this.actualCurrency = actualCurrency;
+    }
+
+    public Stack<Block> getBlockChain() {
+        return blockChain;
+    }
+
+    public void setBlockChain(Stack<Block> blockChain) {
+        this.blockChain = blockChain;
+    }
+
 }
