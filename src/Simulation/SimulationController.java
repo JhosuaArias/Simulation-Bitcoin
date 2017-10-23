1a3f7e8a9914c0f811e8e40b64e9aa540bf42861
JhosuaArias <jhosuarqui@hotmail.com>
2017-10-23 00:25:36 -0600

    Varias simulaciones

--- src/Simulation/SimulationController.java
+++ src/Simulation/SimulationController.java
@@ -2,17 +2,172 @@ package Simulation;
 
 import Node.As;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SimulationController {
 
+    private Scanner keyboard;
 
-    private Simulation simulation;
+    private ArrayList<Simulation> allSimulations;
+
+    private int numberAses;
+    private int numberMiners;
+    private boolean thereIsAttack;
+    private int victimAS;
+    private int simulationTime;
+    private int numberSimulations;
 
     public SimulationController() {
-        this.simulation = new Simulation();
+        this.keyboard = new Scanner(System.in);
+        this.allSimulations = new ArrayList<>();
+
+    }
+
+
+    private void requestVictimAS(int totalASes) {
+        do {
+
+            try {
+                System.out.print("Please enter the AS number you want to isolate (between 1 - " + totalASes + "): ");
+                this.victimAS = Integer.parseInt(keyboard.nextLine());
+
+                if (this.victimAS <= 0 || this.victimAS > totalASes) {
+                    System.out.println("Please enter a number, from 1 to "+ totalASes +".");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+                this.victimAS = -1;
+            }
+
+
+        } while (this.victimAS <= 0 || this.victimAS > totalASes);
+
+        this.victimAS--; //Because the IDs start at 0
+
+    }
+
+    private void requestAttack() {
+        String userChoice;
+
+        do {
+            System.out.print("Do you want to make an partition attack [y/n]: ");
+            userChoice = keyboard.nextLine();
+
+            if (!userChoice.equals("y") && !userChoice.equals("n")) {
+                System.out.println("Please enter a valid option...");
+            }
+
+        } while (!userChoice.equals("y") && !userChoice.equals("n"));
+
+        this.thereIsAttack = userChoice.equals("y");
+    }
+
+    private int requestNumberNodes() {
+        int numberNodes;
+        do {
+
+            try {
+                System.out.print("Please enter the number of nodes you want: ");
+                numberNodes = Integer.parseInt(keyboard.nextLine());
+
+                if (numberNodes <= 0 || numberNodes > 50) {
+                    System.out.println("Please enter a number, from 1 to 50.");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+                numberNodes = -1;
+            }
+
+
+        } while (numberNodes <= 0 || numberNodes > 50);
+
+        return numberNodes;
+    }
+
+    private int requestNumberASes() {
+        int numberAS;
+        do {
+
+            try {
+                System.out.print("Please enter the number of ASes you want: ");
+                numberAS = Integer.parseInt(keyboard.nextLine());
+
+                if (numberAS <= 0 || numberAS > 10) {
+                    System.out.println("Please enter a number, from 1 to 10.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+                numberAS = -1;
+            }
+
+        } while (numberAS <= 0 || numberAS > 10);
+
+        return numberAS;
+    }
+
+    private void requestSimulationTime() {
+        do {
+
+            try {
+                System.out.print("Please enter the time (in minutes) for the simulation: ");
+                this.simulationTime = Integer.parseInt(keyboard.nextLine());
+
+                if (this.simulationTime <= 0) {
+                    System.out.println("Please enter a number bigger than 0.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+                this.simulationTime = -1;
+            }
+
+        } while (this.simulationTime <= 0);
+
+    }
+
+    private void requestNumberSimulations() {
+        do {
+
+            try {
+                System.out.print("Enter the number rof simulations you want to execute: ");
+                this.numberSimulations = Integer.parseInt(keyboard.nextLine());
+
+                if (this.numberSimulations <= 0) {
+                    System.out.println("Please enter a number bigger than 0.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+                this.numberSimulations = -1;
+            }
+
+        } while (this.numberSimulations <= 0);
+
     }
 
     public void init() {
-        simulation.initSimulation();
+
+        this.requestSimulationTime();
+        this.numberAses = requestNumberASes();
+        this.numberMiners = requestNumberNodes();
+        this.requestAttack();
+
+        if (this.thereIsAttack) {
+            requestVictimAS(numberAses);
+        }
+
+        this.requestNumberSimulations();
+
+        for (int i = 0; i < numberSimulations; i++) {
+            this.allSimulations.add(new Simulation(this.numberAses,this.numberMiners,this.thereIsAttack,this.victimAS,this.simulationTime));
+        }
+
+        for (Simulation simulation: this.allSimulations) {
+            simulation.initSimulation();
+        }
+
+
     }
     public static void main(String[] args) {
         SimulationController simulationController;
---
+++ src/Simulation/Statistics.java
@@ -0,0 +1,17 @@
+package Simulation;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Statistics {
+    ArrayList<Double> asesIncomeStatistics;
+
+    public Statistics() {
+        this.asesIncomeStatistics = new ArrayList<>(10);
+    }
+
+    public void addBlockMined(int asId) {
+
+    }
+
+}
